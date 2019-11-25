1b9c12f5e487ef3b947989dcc84446ed430d7400
crandles55 <crandles@missouriwestern.edu>
2019-11-25 12:13:50 -0600

    Update Chuck.kt
    finished game

--- src/view/Chuck.kt
+++ src/view/Chuck.kt
@@ -8,6 +8,7 @@ import tornadofx.*
 import java.util.*
 import java.util.Scanner.*
 import java.util.Arrays
+import java.util.Random.*
 
 var repeat_check = false
 
@@ -18,50 +19,123 @@ fun main(args: Array<String>){
     println("Welcome to Bulls and Cows!")
     println("Please Enter a 5 Letter Word")
 
-    val user_guess = getGuess()
+    var game = BullsAndCows();
+
+    var word_to_guess = game.get_word_to_guess()
+
+    while (game.guess_value != 5 && game.end_game == 0) {
+        var guess = game.getGuess()
+        if (guess.length == 5){
+            game.checkAnswer(guess, word_to_guess)
+        }
+    }
+
 }
 
 
-fun getGuess(): String? {
-    val guess = readLine()
+class BullsAndCows {// Constructor Declaration of Class
 
-    if (guess != null) {
-       repeat_check =  check_for_repeat(guess)
+    var bulls: Int = 0
+    var cows: Int = 0
+    var guess_value: Int = 0
+    var end_game: Int = 0
+
+    fun getBullCount(): Int {
+        return bulls
     }
 
-    if (repeat_check != false) {
+    fun getCowCount(): Int {
+        return cows
+    }
+
+    fun get_word_to_guess(): String{
+
+        var word_to_guess = ""
+
+        val random = Random()
+
+        val number_for_pick = random.nextInt(9)
+
+        val list_of_words = listOf("quick", "quack", "plans", "gnarl", "asked", "honey", "macho", "minus", "pearl", "scarf")[number_for_pick]
+
+        word_to_guess = list_of_words
+
+        //return word_to_guess
+
+        return "quack"
+    }
+
+    fun check_for_repeat(guess: String): Boolean {
+
+        var new_guess = guess.toLowerCase()
+        val len = new_guess.length
+
+        val arr = new_guess.toCharArray()
+
+        Arrays.sort(arr)
+        for (i in 0 until len - 1) {
+            if (arr[i] == arr[i + 1])
+                return false
+        }
+        return true
+
+    }
+
+
+    fun getGuess(): String {
+        val guess = readLine()
+
         if (guess != null) {
-            if (guess.length == 5 ) {
-                println(guess)
-            }
-            else {
-                println("Please Enter a 5 Letter Word")
-                getGuess()
-            }
+            repeat_check =  check_for_repeat(guess)
         }
 
+        if (repeat_check != false) {
+            if (guess != null) {
+                if (guess.length == 5 ) {
+                }
+                else {
+                    println("Please Enter a 5 Letter Word")
+                    //getGuess()
+                    return null.toString()
+                }
+            }
+
+        }
+        else{
+            println("Please Enter a Word Without Repeating Letters")
+            //getGuess()
+            return null.toString()
+        }
+
+        return guess.toString()
     }
-    else{
-        println("Please Enter a Word Without Repeating Letters")
-        getGuess()
+
+    fun checkAnswer(guess: String, correctWord: String) {
+
+        bulls = 0
+        cows = 0
+
+        for (i in 0 until guess.length) {
+            val c = guess[i]
+
+            if (c == correctWord.get(i)) {
+                bulls++
+            } else if (correctWord.contains(c.toString())) {
+                cows++
+            }
+        }
+        if (bulls != 5) {
+            println("You have guessed the word " + guess)
+            println("Bulls: " + bulls)
+            println("Cows: " + cows)
+            guess_value++
+            println("Guess " + guess_value + "/5")
+        }
+        else{
+            end_game++
+            println("You Win!")
+        }
     }
-
-    return guess
-}
-
-fun check_for_repeat(guess: String): Boolean {
-
-    var new_guess = guess.toLowerCase()
-    val len = new_guess.length
-
-    val arr = new_guess.toCharArray()
-
-    Arrays.sort(arr)
-    for (i in 0 until len - 1) {
-        if (arr[i] == arr[i + 1])
-            return false
-    }
-    return true
 
 }
 
