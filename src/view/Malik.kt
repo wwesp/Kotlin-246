e82dae8f3e2add5c48cc5a6e39d2c425c7bab31f
McKFlat <38057035+McKFlat@users.noreply.github.com>
2019-12-04 08:33:51 -0600

    Update Malik.kt
    added Euclidean Algorithm and read 2 numbers from file

--- src/view/Malik.kt
+++ src/view/Malik.kt
@@ -1,16 +1,60 @@
 package view
 
-import tornadofx.*
-import tornadofx.*
-import app.MyApp
 import app.Styles
+import javafx.scene.control.TextArea
 import tornadofx.*
+import java.io.File
+import kotlin.system.exitProcess
 
-class malikMain : View("malikMain") {
+class MalikMain : View("") {
 
+    lateinit var ta: TextArea
     override val root = hbox {
+
+
         label(title) {
             addClass(Styles.heading)
         }
+        pane{
+
+            ta = textarea()
+            ta.isWrapText = true
+
+            ta.translateX = 0.0
+            ta.translateY = 50.0
+            ta.maxWidth = 400.0
+            ta.maxHeight = 800.0
+            ta.text = readFromFile("src/resources/twoNumbers.txt")
+
+        }
     }
+
+
+
+
+    fun gcd2(a:Int, b:Int):Int{
+        if (b==0)
+            return a else return gcd2(b, a%b)
+    }
+
+    fun readFromFile(fileName: String): String {
+
+        //Kotlin
+        val file = fileName
+        try {
+           var lines: String = File(file).readLines().toString()
+            var a = lines.split(" ")
+            var b = a[1].substring(0, 3).toInt()
+            println(b)
+            return "gcd for "+a[0].substring(1)+ " and " + b+ " is " + gcd2(a[0].substring(1).toInt(), b)
+
+
+            return a.toString()
+        }catch(e: Exception){
+            e.printStackTrace()
+            exitProcess(-1)
+        }
+    }
+
 }
+
